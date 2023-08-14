75066d05c5c1d6e1dbb74495a9c87b93991cb99a
zjoker120 <zjoker120@gmail.com>
2023-08-14 13:57:18 +0900

    feat: 4.7 8-14 추가

--- app.js
+++ app.js
@@ -1,8 +1,41 @@
+const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const loginButton = document.querySelector("#login-form button");
+const greeting = document.querySelector("#greeting");
 
-function onLoginBtnClick (){
-    const username = loginInput.value;
-    console.log(username);
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
+function onLoginSubmit(event) {
+event.preventDefault();
+// step1. 화면 새로고침 방지
+loginForm.classList.add(HIDDEN_CLASSNAME);
+// step2. form을 다시 숨겨준다
+const username = loginInput.value;
+// step3. value를 username이라는 key값으로 저장시켜준다
+localStorage.setItem(USERNAME_KEY, username);
+// step4. username값을 username이라는 key와 함께 local storeage에 저장한다
+paintGreetings(username);
+// onLoginSubmit함수에서는 유저정보가 input으로부터 오고 있다
 }
-loginButton.addEventListener("click",onLoginBtnClick)
+
+function paintGreetings(username) {
+greeting.innerText = `Hello ${username}`;
+greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+// step5. 비어있는 h1 요소안에 `Hello username` 이라는 텍스트 추가
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+// step6. 앱이 시작되면 local storage에서 savedusername을 얻으려고 할텐데 거기서 username이라는 key를 가지고 찾게 된다
+
+// step7. 처음에는 key랑 value가 null이므로
+if (savedUsername === null) {
+loginForm.classList.remove(HIDDEN_CLASSNAME);
+// form에 hidden을 지워주고
+loginForm.addEventListener("submit", onLoginSubmit);
+// form이 submit될때만 onLoginSubmit함수를 실행 시키도록 한다
+} else {
+paintGreetings(savedUsername);
+// 유저정보가 localstoreage에서 나오고 있다
+// paintGreeting은 only localstoarage에서만 값을 불러온다
+}
