ecca94af1ad60cec28c85fb06e55a4be582ad8c1
zjoker120 <zjoker120@gmail.com>
2023-08-14 15:12:49 +0900

    feat: 이름 바꾸기 / 시계추가/ index 연결 / 주석추가

---
+++ js/clock.js
@@ -0,0 +1,25 @@
+const clock = document.querySelector("h2#clock");
+
+function getClock(){
+    const date = new Date();
+    const hours = String( date.getHours()).padStart(2,"0");
+    const minutes = String( date.getMinutes()).padStart(2,"0");
+    const seconds = String( date.getSeconds()).padStart(2,"0");
+    clock.innerText = `${hours}:${minutes}:${seconds}`;
+
+}
+
+
+getClock()
+setInterval(getClock,1000);
+
+
+
+
+
+
+
+
+//setInterval(sayHello, 5000); //5초마다 계속
+//setTimeout(sayHello, 5000); //5초후 보여주고 끝
+
